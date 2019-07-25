8c7e2d4b294bbe0dbb85b844020d7b1369d49084
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2019-07-25 15:05:45 +0100

    Added declare method types

--- src/paginate.ts
+++ src/paginate.ts
@@ -7,6 +7,9 @@ import {
 import { Pagination } from "./pagination";
 import { IPaginationOptions } from "./interfaces";
 
+export async function paginate<T>(repository: Repository<T>, options: IPaginationOptions, searchOptions?: FindConditions<T> | FindManyOptions<T>): Promise<Pagination<T>>;
+export async function paginate<T>(queryBuilder: SelectQueryBuilder<T>, options: IPaginationOptions): Promise<Pagination<T>>;
+
 export async function paginate<T>(
   repositoryOrQueryBuilder: Repository<T> | SelectQueryBuilder<T>,
   options: IPaginationOptions,
