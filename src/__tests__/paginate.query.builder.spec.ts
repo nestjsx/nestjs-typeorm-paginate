ddeb0a456e758eeb8c02e0d709b4f17e2c6eb745
ChuongTran <chuongtrh@gmail.com>
2020-04-11 10:41:53 +0700

    Dynamic number of Post with postAmount & remove seed file

--- src/__tests__/paginate.query.builder.spec.ts
+++ src/__tests__/paginate.query.builder.spec.ts
@@ -8,11 +8,46 @@ import { Pagination } from "../pagination";
 import { PostEntity } from "./post.entity";
 import { paginate } from "./../paginate";
 
+async function seedData(postAmount){
+  const category1 = await CategoryEntity.create({name: 'category 1'}).save();
+  const category2 = await CategoryEntity.create({name: 'category 2'}).save();
+  for(let i =0; i< postAmount; i++){
+    let post = await PostEntity.create({title: `Title ${i+1}`, categories: [category1, category2]}).save()
+    
+    await CommentEntity.create({
+      postId: post.id,
+      text: `Comment 1 of post ${i+1}`
+    }).save()
+    await CommentEntity.create({
+      postId: post.id,
+      text: `Comment 2 of post ${i+1}`
+    }).save()
+  }
+}
+
+async function cleanUp(connection){
+  await connection
+    .createQueryBuilder()
+    .delete()
+    .from(CommentEntity)
+    .execute();
+  await connection
+    .createQueryBuilder()
+    .delete()
+    .from(CategoryEntity)
+    .execute();
+  await connection
+    .createQueryBuilder()
+    .delete()
+    .from(PostEntity)
+    .execute();
+}
+
 describe("Paginate with queryBuilder", () => {
   let app: TestingModule;
   let connection: Connection;
   let queryBuilder: SelectQueryBuilder<PostEntity>;
-
+  let postAmount = 15;
   beforeAll(async ()=>{
     app = await Test.createTestingModule({
       imports: [
@@ -29,9 +64,11 @@ describe("Paginate with queryBuilder", () => {
       ]
     }).compile();
     connection = app.get(getConnectionToken());
+    await seedData(postAmount)
   });
   
-  afterAll(()=>{
+  afterAll(async ()=>{
+    await cleanUp(connection)
     app.close();
   });
 
@@ -41,18 +78,24 @@ describe("Paginate with queryBuilder", () => {
     expect(result).toBeInstanceOf(Pagination);
   });
   
-  it("Query page 1", async () => {
+  it("Query page 1 use QueryBuilder with relations", async () => {
     queryBuilder = connection.createQueryBuilder(PostEntity, "post").leftJoinAndSelect("post.comments", "text").leftJoinAndSelect("post.categories", "name")
-    const result = await paginate(queryBuilder, { limit: 10, page: 1 });
-    expect(result.meta.totalPages).toEqual(2);
-    expect(result.meta.itemCount).toEqual(10);
-    expect(result.meta.totalItems).toEqual(15);
+    
+    let limit = 10;
+    let totalPages = Math.ceil(postAmount/limit);
+    const result = await paginate(queryBuilder, { limit: limit, page: 1 });
+    expect(result.meta.totalPages).toEqual(totalPages);
+    expect(result.meta.totalItems).toEqual(postAmount);
+    expect(result.meta.currentPage).toEqual(1);
   });
-  it("Query page 2", async () => {
+  it("Query page 2 use QueryBuilder with relations", async () => {
     queryBuilder = connection.createQueryBuilder(PostEntity, "post").leftJoinAndSelect("post.comments", "text").leftJoinAndSelect("post.categories", "name")
-    const result = await paginate(queryBuilder, { limit: 10, page: 2 });
-    expect(result.meta.totalPages).toEqual(2);
-    expect(result.meta.itemCount).toEqual(5);
-    expect(result.meta.totalItems).toEqual(15);
+    
+    let limit = 10;
+    let totalPages = Math.ceil(postAmount/limit);
+    const result = await paginate(queryBuilder, { limit: limit, page: 2 });
+    expect(result.meta.totalPages).toEqual(totalPages);
+    expect(result.meta.totalItems).toEqual(postAmount);
+    expect(result.meta.currentPage).toEqual(2);
   });
 });
