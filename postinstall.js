dfa08867bcab3344b619b4107540ae1e9a55a932
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-08-12 09:33:39 +0100

    chore: added postinstall script for poll

---
+++ postinstall.js
@@ -0,0 +1,7 @@
+var chalk = require('chalk')
+
+console.log(
+  chalk.green('[nestjs-typeorm-paginate] =>'),
+  'Please vote for your prefered method of default querying!',
+  chalk.blue('https://github.com/nestjsx/nestjs-typeorm-paginate/issues/576'),
+)
