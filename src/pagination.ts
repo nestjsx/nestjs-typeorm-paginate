49b7fa426c5ab32cf3871a7b2029ccc27d2aa5f2
Hugo Denizart <hugo@denizart.pro>
2021-12-06 20:13:37 +0100

    ✨ Add Expose decorator on all fields

--- src/pagination.ts
+++ src/pagination.ts
@@ -1,21 +1,29 @@
+import { Expose } from 'class-transformer';
 import { IPaginationLinks, IPaginationMeta, ObjectLiteral } from './interfaces';
 
 export class Pagination<
   PaginationObject,
   T extends ObjectLiteral = IPaginationMeta,
 > {
-  constructor(
-    /**
-     * a list of items to be returned
-     */
-    public readonly items: PaginationObject[],
-    /**
-     * associated meta information (e.g., counts)
-     */
-    public readonly meta: T,
-    /**
-     * associated links
-     */
-    public readonly links?: IPaginationLinks,
-  ) {}
+  /**
+   * a list of items to be returned
+   */
+  @Expose()
+  public readonly items: PaginationObject[];
+  /**
+   * associated meta information (e.g., counts)
+   */
+  @Expose()
+  public readonly meta: T;
+  /**
+   * associated links
+   */
+  @Expose()
+  public readonly links?: IPaginationLinks;
+
+  constructor(items: PaginationObject[], meta: T, links?: IPaginationLinks) {
+    this.items = items;
+    this.meta = meta;
+    this.links = links;
+  }
 }
