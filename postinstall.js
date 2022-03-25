4b086ca673fbfea18a4c564e4cb8278a00c67edd
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-03-25 09:04:22 +0000

    feat: remove chalk and postinstall script

--- postinstall.js
+++
@@ -1,7 +0,0 @@
-var chalk = require('chalk')
-
-console.log(
-  chalk.green('[nestjs-typeorm-paginate] =>'),
-  'Please vote for your prefered method of default querying!',
-  chalk.blue('https://github.com/nestjsx/nestjs-typeorm-paginate/issues/576'),
-)
